83d160a104bd4331566e381242dc1d90beca50b0
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2020-11-30 17:50:53 +0100

    Add files via upload

---
+++ 28.py
@@ -0,0 +1,11 @@
+partecipanti = []
+lanci = []
+totpartecipanti = int(input("quanti sono i partecipanti?")
+for n in range(1.totpartecipanti +1):
+    partecipante = input("nome partecipante")
+    lancio = float(input(" quanto è stato il lancio")
+    partecipanti.append(partecipante)
+    lanci.append(lancio)
+print(partecipanti)
+print(lanci)
+print(max(lanci))    
